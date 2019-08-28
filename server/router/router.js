741c2423681cb4c5c9b67d0b6383050a1f0d1c1c
Ally4 <el.ally741@gmail.com>
2019-08-28 18:04:30 +0200

    for the first test

--- server/router/router.js
+++ server/router/router.js
@@ -1,7 +1,12 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable spaced-comment */
 import express from 'express';
+import signup from '../controllers/signup';
 
 const router = express.Router();
+
+//For the signup
+router.post('/api/v1/auth/signup', signup);
 
 export default router; 
---
+++ server/test/testSignup.js
@@ -0,0 +1,72 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+chai.use(chaiHttp);
+chai.should();
+describe('Signup test', () => {
+it('should be able to signup with full information', (done) => {
+    const user = {
+      firstName: 'Nengo',
+      lastName: 'Bin',
+      email: 'bin@gmail.com',
+      password: '123456',
+      address: 'Rwanda',
+      bio: 'Teacher',
+      occupation: 'developer',
+      expertise: 'Public-speaker',
+    };
+    chai.request(app)
+      .post('/api/v1//auth/signup')
+      .send(user)
+      .then((res) => {
+        res.body.status.should.be.equal(201);
+        done();
+      })
+      .catch(err => done(err));
+  });
+      it('should not be able to signup for duplicate', (done) => {
+    const user = {
+      firstName: 'Bonheur',
+      lastName: 'Mwanamboka',
+      email: 'bonheur@gmail.com',
+      password: '123456',
+      address: 'Rwanda',
+      bio: 'Teacher',
+      occupation: 'developer',
+      expertise: 'Public-speaker',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then((res) => {
+        res.body.status.should.be.equal(409);
+        done();
+      })
+      .catch(err => done(err));
+  });
+      it('should not be able to signup when the is no required input information', (done) => {
+    const user = {
+      firstName: 'bonth',
+      lastName: 'Mwanamboka',
+      email: 'bonheurgmail.com',
+      password: '123456',
+      address: 'Rwanda',
+      bio: 'Teacher',
+      occupation: 'developer',
+      expertise: 'Public-speaker',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then((res) => {
+        res.body.status.should.be.equal(400);
+        done();
+      })
+      .catch(err => done(err));
+  });
+});
