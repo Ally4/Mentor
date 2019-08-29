adcdc3b367eb8f9870af27b39212711a3c2a93d2
Ally4 <el.ally741@gmail.com>
2019-08-29 11:25:18 +0200

    feature(signup): signup [finishes #167937331, #168161752]

--- server/router/router.js
+++ server/router/router.js
@@ -2,9 +2,12 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable spaced-comment */
 import express from 'express';
+import signup from '../controllers/signup';
 
 
 const router = express.Router();
 
+//For the signup
+router.post('/api/v1/auth/signup', signup);
 
 export default router; 
