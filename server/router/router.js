54e9bb060fb75690b8ce97eac1efa7a586e5e19e
Ally4 <el.ally741@gmail.com>
2019-08-29 14:56:15 +0200

    feature(mentors): getting-mentors [finishes #167937308]

--- server/router/router.js
+++ server/router/router.js
@@ -3,11 +3,16 @@
 /* eslint-disable spaced-comment */
 import express from 'express';
 import signup from '../controllers/signup';
+import getMentors from '../controllers/mentors';
+import authent from '../middleware/authent';
 
 
 const router = express.Router();
 
 //For the signup
 router.post('/api/v1/auth/signup', signup);
+
+//for all the mentors
+router.get('/api/v1/mentors', authent, getMentors);
 
 export default router; 
