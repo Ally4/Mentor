ebc3637ea3bae32e7b43435a136d5756430d43d9
Ally4 <el.ally741@gmail.com>
2019-08-29 15:48:15 +0200

    feature(mentor): mentor-by-id [finishes #167937301]

--- server/router/router.js
+++ server/router/router.js
@@ -3,11 +3,16 @@
 /* eslint-disable spaced-comment */
 import express from 'express';
 import signup from '../controllers/signup';
+import getAMentor from '../controllers/mentor';
+import authent from '../middleware/authent';
 
 
 const router = express.Router();
 
 //For the signup
 router.post('/api/v1/auth/signup', signup);
+
+//for a mentor by id
+router.get('/api/v1/mentors/:mentorId', authent, getAMentor);
 
 export default router; 
