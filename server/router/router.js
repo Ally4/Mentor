9c49683ec083438d07745ee9c176555b495c6ca3
Ally4 <el.ally741@gmail.com>
2019-08-29 12:07:57 +0200

    feature(signin): sign-and-test [finishes #167937320]

--- server/router/router.js
+++ server/router/router.js
@@ -2,9 +2,13 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable spaced-comment */
 import express from 'express';
+import signin from '../controllers/signin';
 
 
 const router = express.Router();
+
+//for the signin
+router.post('/api/v1/auth/signin', signin);
 
 
 export default router; 
